1bca2c13e53911debf47d2d986921645dd4ccae7
keyhamilton <49598282+keyhamilton@users.noreply.github.com>
2022-09-19 22:32:59 -0300

    Create questao7.ts

---
+++ questao7.ts
@@ -0,0 +1,11 @@
+let exemplo: string;
+let nome: string = 'Ely';
+let numero: number = 120.56;
+let linguagem: string = 'TypeScript'
+
+exemplo = `${nome}
+            My payment time is ${numero}
+            and
+            my preffered language is ${linguagem}`
+
+console.log(exemplo);
